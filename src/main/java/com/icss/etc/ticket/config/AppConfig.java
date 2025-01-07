971921c4de0f769683379335e732b9896647e7d4
SevenThRe <zxcvrrgty2@foxmail.com>
2025-01-07 13:38:19 +0800

    暂时关闭拦截器

--- src/main/java/com/icss/etc/ticket/config/AppConfig.java
+++ src/main/java/com/icss/etc/ticket/config/AppConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-
+    private final AuthInterceptor authInterceptor;
     public AppConfig(AuthInterceptor authInterceptor) {
         this.authInterceptor = authInterceptor;
     }
@@ -36,19 +36,18 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/attachment/**").addResourceLocations("classpath:/static/attachment/");
     }
 
-    /**
-     * TODO: 身份验证拦截器
-     * 结合JWT实现身份验证
-     */
-    private final AuthInterceptor authInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 添加登录认证拦截器
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/**")   // 拦截所有请求
-                .excludePathPatterns("/pages/auth/**")
-                .excludePathPatterns("/api/auth/**");   // 排除登录注册等不需要验证的路径
-
-    }
+//    /**
+//     * TODO: 身份验证拦截器
+//     * 结合JWT实现身份验证
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 添加登录认证拦截器
+//        registry.addInterceptor(authInterceptor)
+//                .addPathPatterns("/**")   // 拦截所有请求
+//                .excludePathPatterns("/pages/auth/**")
+//                .excludePathPatterns("/api/auth/**");   // 排除登录注册等不需要验证的路径
+//
+//    }
 
 }
