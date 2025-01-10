fefef1d7046fbacf04896dc7d0cb67af495ddde0
Administrator <784261156@qq.com>
2025-01-10 11:39:19 +0800

    Revert "删除event bus，改为jQuery事件绑定"
    This reverts commit 6304db0f0759d01242a32e3b01168d1a15be1435.

--- src/main/java/com/icss/etc/ticket/controller/UserController.java
+++ src/main/java/com/icss/etc/ticket/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.icss.etc.ticket.controller;
 
 import com.icss.etc.ticket.entity.R;
-import com.icss.etc.ticket.entity.vo.DeptMemberVO;
 import com.icss.etc.ticket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,21 +31,7 @@ public class UserController {
         return R.OK(userService.getUsernames());
     }
 
-    /**
-     * 获取处理人列表
-     * @param departmentId 部门ID
-     * @return 用户ID列表
-     */
-    @GetMapping("/selectByDepartmentId/{department_id}")
-    public R<List<DeptMemberVO>> selectByDepartmentId(@PathVariable("department_id") Long departmentId) {
-        return R.OK(userService.selectByDepartmentId(departmentId));
-    }
 
-    /**
-     * 获取用户信息
-     * @param userId 用户ID
-     * @return 用户信息
-     */
-    @GetMapping("/current")
+
 
 }
